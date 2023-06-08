717e7889907b6da1811a741f1ad41674aa4fbceb
Olga Forostinko <oforostinko@gmail.com>
2023-06-08 10:08:30 -0700

    added request fetch and axios

---
+++ part1/src/components/BookList/BookDetails/Axios.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+import parse from 'html-react-parser';
+import axios from 'axios';
+
+function BookDetails({ bookId}) {
+	const [book, setBook] = useState(null);
+	const [error, setError] = useState(false);
+
+	useEffect(() => {
+		axios(`https://www.googleapis.com/books/v1/volumes/${bookId}`)
+            .then((response) => {
+                console.log(response);
+                setBook(response.data);
+				setError(false);
+            }) 
+			.catch((error) => {
+				console.error(error);
+				setError(true);
+			});
+	}, [bookId]);
+
+    if (!book) {
+        return <div>Loading...</div>
+    }
+
+	if (error) {
+		return <div className="container">Book not found.</div>
+	}
+
+	return (
+		<div className="book">
+			<h1>{book.volumeInfo.title ? book.volumeInfo.title : 'No title' }</h1>
+			<h2>{book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'No authors'}</h2>
+			<div>{book.volumeInfo.description ? parse(book.volumeInfo.description) : 'No description available'}</div>
+			{book.volumeInfo.imageLinks && <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />}
+		</div>
+	);
+}
+
+export default BookDetails;
